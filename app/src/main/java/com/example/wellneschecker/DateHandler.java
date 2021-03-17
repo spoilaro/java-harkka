684f5c75028758a1d04120b8f5e9838b70a86141
Leevi Malinen <joona.mlin@gmail.com>
2021-03-17 13:02:22 +0200

    Added DateHandler

---
+++ app/src/main/java/com/example/wellneschecker/DateHandler.java
@@ -0,0 +1,38 @@
+package com.example.wellneschecker;
+
+import java.util.Calendar;
+import java.util.ArrayList;
+
+
+class DateHandler{
+
+    static int currentDay;
+    static ArrayList<Integer> weekDays;
+
+    DateHandler(){     //TODO Make it return right value when month changes!!
+
+        //Declarations and initializing
+        Calendar calendar = Calendar.getInstance();
+        weekDays = new ArrayList<Integer>();
+
+        //Current time
+        currentDay = calendar.get(Calendar.DAY_OF_MONTH);
+
+        // Set the time to this week's monday
+        calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+
+        //Add dates to array
+        for (int i=0; i<7; i++){
+            weekDays.add(calendar.get(Calendar.DAY_OF_MONTH));
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
+        }
+    }
+
+    public int getCurrentDay(){
+        return currentDay;
+    }
+
+    public ArrayList<Integer> getWeekDates(){
+        return weekDays;
+    }
+}
