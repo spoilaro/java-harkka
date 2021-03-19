30a15144b54ec7367b2f1232d65c63c6e4ca9b1f
Johannes <johannes.haapanen@gmail.com>
2021-03-19 13:02:57 +0200

    fixes

---
+++ app/src/main/java/com/example/wellneschecker/UserProfileHandler.java
@@ -0,0 +1,11 @@
+package com.example.wellneschecker;
+
+import org.json.JSONObject;
+
+public class UserProfileHandler {
+    UserProfile currentUserProfile;
+
+    UserProfileHandler(){
+        
+    }
+}
