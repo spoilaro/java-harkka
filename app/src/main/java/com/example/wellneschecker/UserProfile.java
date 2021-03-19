873901a9298c474ed8bfecba07a1f36ca4ba0c51
Johannes <johannes.haapanen@gmail.com>
2021-03-19 12:37:02 +0200

    Added basic user profile

---
+++ app/src/main/java/com/example/wellneschecker/UserProfile.java
@@ -0,0 +1,14 @@
+package com.example.wellneschecker;
+
+public class UserProfile {
+    String user_email;
+    String password;
+    String user_name;
+
+    int excercise_amount_max;
+    int excercise_amount_min;
+
+    //TODO TIME ATTRIBUTE
+
+
+}
