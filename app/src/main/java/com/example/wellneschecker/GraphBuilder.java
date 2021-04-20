4ac7c48c02574423d9c560eef5ce14550dc06532
spoilaro <johannes.haapanen@gmail.com>
2021-04-20 16:48:11 +0300

    added writing

--- app/src/main/java/com/example/wellneschecker/GraphBuilder.java
+++ app/src/main/java/com/example/wellneschecker/GraphBuilder.java
@@ -1,4 +1,22 @@
 package com.example.wellneschecker;
 
+import android.content.Context;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class GraphBuilder {
+
+    void createDefaultCSV(Context context) throws IOException {
+        File file = new File(context.getFilesDir(), String.format("move.csv"));
+        FileWriter fileWriter = new FileWriter(file);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.write("1;0;1;2;3" +
+                "2;3;1;0;4");
+        bufferedWriter.close();
+        System.out.println("#########DONE CSV##########");
+    }
+
 }
