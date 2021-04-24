72cb23195141d022a083232260b4913f89276bfd
Leevi Malinen <joona.mlin@gmail.com>
2021-04-24 19:52:49 +0300

    Removed most of settings activity as it was unneeded

--- app/src/main/java/com/example/wellneschecker/SettingsActivityS.java
+++ app/src/main/java/com/example/wellneschecker/SettingsActivityS.java
@@ -13,84 +13,14 @@ import android.widget.TextView;
 
 public class SettingsActivityS extends AppCompatActivity {
 
-    Button addMinMax;
-    SeekBar max;
-    SeekBar min;
-    int maxHours;
-    int minHours;
-
-    Context context;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_s);
-
-        //addMinMax = findViewById(R.id.minmaxbutton);
-
-        //setMaxListener();
-        //setMinListener();
-        //setButtonListener();
-
-        context = getApplicationContext();
     }
 
     public void changeToHome(View v){
         Intent homeIntent = new Intent(this, MainActivity.class);
         startActivity(homeIntent);
     }
-
-    public void getMinMax(){
-        System.out.println(String.format("max is %d and min is %d", maxHours, minHours));
-    }
-
-    void setMaxListener(){
-        //max = findViewById(R.id.seekBarMax);
-        max.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                maxHours = progress;
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-    }
-
-    void setMinListener(){
-        //min = findViewById(R.id.seekBarMin);
-        min.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                minHours = progress;
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-    }
-
-    void setButtonListener(){
-        addMinMax.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getMinMax();
-            }
-        });
-    }
-
 }
