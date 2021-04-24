e34a8ad0904b462f343c8ff019fc8ef727eceb5b
Johannes <johannes.haapanen@gmail.com>
2021-04-24 13:22:00 +0300

    trying to add shared preferences

--- app/src/main/java/com/example/wellneschecker/SettingsActivityS.java
+++ app/src/main/java/com/example/wellneschecker/SettingsActivityS.java
@@ -2,11 +2,14 @@ package com.example.wellneschecker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class SettingsActivityS extends AppCompatActivity {
 
@@ -15,6 +18,8 @@ public class SettingsActivityS extends AppCompatActivity {
     SeekBar min;
     int maxHours;
     int minHours;
+
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,8 @@ public class SettingsActivityS extends AppCompatActivity {
         setMaxListener();
         setMinListener();
         setButtonListener();
+
+        context = getApplicationContext();
     }
 
     public void changeToHome(View v){
@@ -84,6 +91,15 @@ public class SettingsActivityS extends AppCompatActivity {
                 getMinMax();
             }
         });
+    }
+
+    public void setPlace(View view){
+        TextView placeinput = findViewById(R.id.editText_Location);
+
+        SharedPreferences sharedPref = context.getSharedPreferences("wellnescheckersettings", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("place", placeinput.getText().toString());
+        editor.apply();
     }
 
 }
