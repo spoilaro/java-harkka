aeff35ed7e5c83d61c302336b99163d2177ff813
Johannes <johannes.haapanen@gmail.com>
2021-04-26 00:44:49 +0300

    Added basic local user sign up and sign in

--- app/src/main/java/com/example/wellneschecker/LogInActivity.java
+++ app/src/main/java/com/example/wellneschecker/LogInActivity.java
@@ -2,20 +2,31 @@ package com.example.wellneschecker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class LogInActivity extends AppCompatActivity {
 
     TextView usernameBox;
     TextView passwordBox;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        context = getApplicationContext();
     }
 
     public void logIn(View v){
@@ -34,11 +45,40 @@ public class LogInActivity extends AppCompatActivity {
     }
 
     boolean checkCredentials(String username, String password){
-        if (username.equals("admin") && password.equals("admin")){
+        String ret = "";
+
+        try {
+            InputStream inputStream = context.openFileInput(String.format("%s.txt", username));
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+            return false;
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+            return false;
+        }
+
+        String[] readUserPassword = ret.split(" ");
+        if (readUserPassword[1].equals(password)){
             return true;
         } else {
             return false;
         }
+
     }
 
     public void changeToCreateUser(View v){
